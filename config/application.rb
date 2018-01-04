5213d42d58b699ac0b68d04bf254eaaa0f640248
Eduard Krigulson <ekrigulson@gmail.com>
2018-01-04 14:01:48 +0200

    Changed dotenv load order

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Dotenv::Railtie.load
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
 
 module KuhuSooma
   class Application < Rails::Application
